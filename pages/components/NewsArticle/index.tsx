35f6206748e181013c1de1b51460d320c5df532a
lance13c <pianoman13c@gmail.com>
2020-09-06 19:41:14 -0400

    initializes NewsArticle and NewsContainer

--- pages/components/NewsArticle/index.tsx
+++ pages/components/NewsArticle/index.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import styled from 'styled-components';
 
 export interface NewsArticleProps {
-	title?: string;
+	title: string;
 	subTitle?: string;
 	date?: string;
 	content?: string;
--- pages/components/NewsContainer/index.tsx
+++ pages/components/NewsContainer/index.tsx
@@ -1,9 +1,26 @@
 import * as React from 'react';
+import styled from 'styled-components';
+import NewsArticle, { NewsArticleProps } from '../NewsArticle';
 
-export interface NewsContainerProps {}
+export interface NewsContainerProps {
+	newsArticles: NewsArticleProps[];
+}
 
-const NewsContainer: React.SFC<NewsContainerProps> = () => {
-	return <div />;
+const Container = styled.ul`
+	display: flex;
+	flex-wrap: wrap;
+`;
+
+const NewsContainer: React.SF<NewsContainerProps> = ({ newsArticles }) => {
+	return (
+		<Container>
+			{() => {
+				return newsArticles.map((article: NewsArticleProps) => {
+					return <NewsArticle key={article.title} {...article} />;
+				});
+			}}
+		</Container>
+	);
 };
 
 export default NewsContainer;
